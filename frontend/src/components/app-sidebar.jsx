3c5412c1cbb61b25d253bd4c6f2cd6d4e3d5eea6
Ali Shanawer <alishanawer2004@gmail.com>
2025-11-02 20:17:05 +0500

    Updated the design of the app

--- frontend/src/components/app-sidebar.jsx
+++ frontend/src/components/app-sidebar.jsx
@@ -52,8 +52,12 @@ export function AppSidebar({ ...props }) {
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
               <Link to="/">
-                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                  <Command className="size-4" />
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-accent">
+                  <img
+                    src="/evently.svg"
+                    alt="Evently Logo"
+                    className="size-6"
+                  />
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">Evently</span>
